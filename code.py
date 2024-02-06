22b485028f222d1efd5b4f847ce56c1a3a845c31
SuperHands <anthon3869@gmail.com>
2024-02-06 07:57:21 -0600

    Update code.py

--- code.py
+++ code.py
@@ -1,22 +1,51 @@
 """
-    GP0:  PLAYER_RX (DFPlayer Mini RX)
-    GP1:  PLAYER_TX (DFPlayer Mini TX)
-    GP2:  SERVO_PIN (Wing Servo)
-    GP4:  I2C_SDA (I2C SDA connection for LSM9DS1)
-    GP5:  I2C_SCL (I2C SCL connection for LSM9DS1) 
-    GP10: BUTTON_SENSITIVITY_DECREASE (Decrease signal detetection sensitivity value) (14)
-    GP11: BUTTON_SENSITIVITY_INCREASE (Increase signal detetection sensitivity value) (15)
-    GP12: BUTTON_GAIN_DECREASE (Decrease signal gain value) (16)
-    GP13: BUTTON_GAIN_INCREASE (Increase signal gain value) (17)
-    GP14: LED_INCREASE (Signal adjustment increased indicator LED) (19)
-    GP15: LED_DECREASE (Signal adjustment decreased indicator LED) (20)
-    GP16: LED_WING_1 (Wing/Display LED 1) GREEN
-    GP17: LED_WING_2 (Wing/Display LED 2) BLUE
-    GP18: LED_WING_3 (Wing/Display LED 3) WHITE
-    GP19: LED_WING_4 (Wing/Display LED 4) RED
-    GP20: LED_WING_5 (Wing/Display LED 5) YELLOW (R)
-    GP21: LED_WING_6 (Wing/Display LED 6) BLACK
-    GP22: LED_WING_7 (Wing/Display LED 7) YELLOW
+
+  Project Name: Ghostbusters "Functional" P.K.E Meter
+  Author: Ant3869
+ 
+  Description:
+    This project is a functional replica of the P.K.E. Meter from the movie Ghostbusters. 
+    It not only mimics the iconic design but also serves as a real Electromagnetic Field (EMF) detector.
+
+    Features
+        Functioning EMF detection
+        Ability to increase/decrease sensitivity/range (using the 4 Direct Contact Touch Switches)
+        Wings that raise or lower (reflecting detection: Higher the reading, higher the raise, and vice versa)
+        7 LEDs on each wing blink in sequence (same as in the film Ghostbusters)
+        LEDs speed increase and decrease (reflecting detection: Higher the reading, faster the speed, and vice versa)
+        3 unique SFX while detecting (low, medium, and high readings)
+
+    Components:
+        Raspberry Pi Pico (3.3V)
+        LSM9DS1 D0F 3-axis Accel/Mag/Gyro
+        DFPlayer Mini
+        5V 2A Recharge/Boost Converter Power Module
+        SURPASS 9g S0009M Servo Metal Gear Motor
+        iPhone 5 Loudspeaker
+        33 LEDs (7 on each 'wing', 14 on display screen, and 5 indicators on front panel)
+        16mm Variable Resistor Potentiometer
+        4 Direct Contact Touch Switches
+        3.7v Lithium Rechargeable Battery
+        
+    Hardware Connections (pin configurations):
+        GP0:  PLAYER_RX (DFPlayer Mini RX)
+        GP1:  PLAYER_TX (DFPlayer Mini TX)
+        GP2:  SERVO_PIN (Wing Servo)
+        GP4:  I2C_SDA (I2C SDA connection for LSM9DS1)
+        GP5:  I2C_SCL (I2C SCL connection for LSM9DS1) 
+        GP10: BUTTON_SENSITIVITY_DECREASE (Decrease signal detetection sensitivity value) (14)
+        GP11: BUTTON_SENSITIVITY_INCREASE (Increase signal detetection sensitivity value) (15)
+        GP12: BUTTON_GAIN_DECREASE (Decrease signal gain value) (16)
+        GP13: BUTTON_GAIN_INCREASE (Increase signal gain value) (17)
+        GP14: LED_INCREASE (Signal adjustment increased indicator LED) (19)
+        GP15: LED_DECREASE (Signal adjustment decreased indicator LED) (20)
+        GP16: LED_WING_1 (Wing/Display LED 1) GREEN
+        GP17: LED_WING_2 (Wing/Display LED 2) BLUE
+        GP18: LED_WING_3 (Wing/Display LED 3) WHITE
+        GP19: LED_WING_4 (Wing/Display LED 4) RED
+        GP20: LED_WING_5 (Wing/Display LED 5) YELLOW (R)
+        GP21: LED_WING_6 (Wing/Display LED 6) BLACK
+        GP22: LED_WING_7 (Wing/Display LED 7) YELLOW
     
 """
 
@@ -61,17 +90,12 @@ BUTTON_GAIN_DECREASE  = board.GP12
 BUTTON_GAIN_INCREASE  = board.GP13    
 
 # --- constants/variables   ----------------------------------------------------------
-SERVO_ANGLE_MIN  = 0
-SERVO_ANGLE_MAX  = 180
-SERVO_PULSE_MIN  = 750
-SERVO_PULSE_MAX  = 1800
-SERVO_RAMP_DURATION = 2 # Duration in seconds
+player_baud      = 9600
+player_vol       = 80
+current_track     = None
 
-PLAYER_BAUD      = 9600
-PLAYER_VOL       = 80
-
-EMF_MIN_µT       = 0.0           # Minimum expected EMF reading
-EMF_MAX_µT       = 100.0         # Maximum expected EMF reading
+emf_min_µt       = 0.0           # Minimum expected EMF reading
+emf_max_µt       = 100.0         # Maximum expected EMF reading
 EMF_MIN_ADC      = 0
 EMF_MAX_ADC      = 1100
 
@@ -87,8 +111,9 @@ MAX_GRAPH_VALUE   = 1100          # Maximum value after scaling
 OLED_WIDTH        = 128
 OLED_HEIGHT       = 64
 GRAPH_BASELINE_Y  = OLED_HEIGHT-6 # Baseline for the graph (e.g., half the OLED height)
+
+calibration_time  = 10
 window_size       = 10            # Default window size for smoothing
-current_track     = None
 
 # sensitivity
 sensitivity = 0.5  # Starting sensitivity, adjust as needed
@@ -110,7 +135,7 @@ debounce_delay = 0.05  # 50 milliseconds for debounce
 
 # --- objects   -----------------------------------------------------------
 i2c       = busio.I2C(I2C_SCL, I2C_SDA)
-uart      = busio.UART(tx=PLAYER_TX, rx=PLAYER_RX, baudrate=PLAYER_BAUD) # Using UART0 on default pins GP0 (TX)and GP1 (RX)
+uart      = busio.UART(tx=PLAYER_TX, rx=PLAYER_RX, baudrate=player_baud) # Using UART0 on default pins GP0 (TX)and GP1 (RX)
 
 led_increased   = digitalio.DigitalInOut(LED_INCREASED)
 led_decreased   = digitalio.DigitalInOut(LED_DECREASED)
@@ -120,12 +145,12 @@ button_decrease = digitalio.DigitalInOut(BUTTON_SENSITIVITY_DECREASE)      # Exa
 
 oled           = SSD1306_I2C(OLED_WIDTH, OLED_HEIGHT, i2c)
 sensor         = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
-led_controller = LEDController(LED_PINS, pwm=False, emf_min=EMF_MIN_ADC, emf_max=EMF_MAX_ADC, led_slow=LED_SLOW, led_fast=LED_FAST)
-servo_ramp     = ServoRamp(servo_pin=SERVO_PIN, min_pulse=SERVO_PULSE_MIN, max_pulse=SERVO_PULSE_MAX, default_movement_duration=SERVO_RAMP_DURATION)
-sensor_manager = SensorManager(sensor, oled, servo_ramp, calibration_time=10)
+led_controller = LEDController(LED_PINS, pwm=False)
+servo_ramp     = ServoRamp(servo_pin=SERVO_PIN)
+sensor_manager = SensorManager(sensor, oled, servo_ramp)
 
 # --- initialization   ----------------------------------------------------
-y_history      = [OLED_HEIGHT // 6] * OLED_WIDTH    # Mid-screen baseline
+y_history = [OLED_HEIGHT // 6] * OLED_WIDTH    
 
 led_increased.direction   = digitalio.Direction.OUTPUT
 led_decreased.direction   = digitalio.Direction.OUTPUT
@@ -135,14 +160,12 @@ button_decrease.pull      = digitalio.Pull.UP
 button_increase.direction = digitalio.Direction.INPUT
 button_increase.pull      = digitalio.Pull.UP
 
-sensor_manager.start_calibration()
-
 try:
     uart.write(b'\x7E\xFF\x06\x0C\x00\x00\x00\x00\xEF') #DFPlayer Mini to reset
     time.sleep(2)
     dfplayer = DFPlayer(uart=uart)
     dfplayer.set_eq(DFPlayer.EQ_NORMAL)
-    dfplayer.set_volume(PLAYER_VOL)
+    dfplayer.set_volume(player_vol)
     print("DFPlayer initialized successfully.")
 except Exception as e:
     print("Failed to initialize DFPlayer:", e)
@@ -161,48 +184,7 @@ def update_history(new_value):
 def map_value(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
-"""
-
-# Function for a simple moving average
-def simple_moving_average(history, window_size=10):
-    return [sum(history[max(i - window_size, 0):i+1]) / min(i + 1, window_size) for i in range(len(history))]
-
-# Function to process magnetometer data
-def process_mag_data(mx, my, mz, baseline):
-    emf_strength = math.sqrt(mx**2 + my**2 + mz**2) - baseline
-    mapped_emf = map_value(emf_strength, EMF_MIN_µT, EMF_MAX_µT, EMF_MIN_ADC, EMF_MAX_ADC)
-    processed_value = (mapped_emf ** 3)
-    return max(0, min(1100, processed_value))
-
-# Function to process and smooth magnetometer data
-def smooth_and_process_data(mx, my, mz, baseline, history, window_size=10):
-    emf_strength = math.sqrt(mx**2 + my**2 + mz**2)
-    relative_strength = emf_strength - baseline
-    mapped_emf = map_value(relative_strength, EMF_MIN_µT, EMF_MAX_µT, EMF_MIN_ADC, EMF_MAX_ADC)
-    history.append(mapped_emf)
-    
-    if len(history) > window_size:
-        history.pop(0)
-    
-    smoothed_value = sum(history[-window_size:]) / len(history[-window_size:])
-    return max(0, min(1100, smoothed_value))
-
-# Function to control LEDs
-def dynamic_led_control(emf_level):
-    intensity = map_value(emf_level, EMF_MIN_ADC, EMF_MAX_ADC, LED_INTENSITY_MIN, LED_INTENSITY_MAX)
-    intensity = max(LED_INTENSITY_MIN, min(LED_INTENSITY_MAX, intensity)) # Clamp intensity to [LED_INTENSITY_MIN, LED_INTENSITY_MAX] range
-    blink_speed = map_value(intensity, 0, 1, LED_SLOW, LED_FAST)
-    
-    duty_cycle = floor(intensity * 65535)        # Calculate duty_cycle within 0-65535 range
-    duty_cycle = max(0, min(65535, duty_cycle))  # Additional clamping for safety
-    led_wing1_pwm.duty_cycle = duty_cycle
-
-    # Keep the LED on for a fraction of the blink speed, then turn off
-    time.sleep(blink_speed / 5)  # On time
-    led_wing1_pwm.duty_cycle = 0
-    time.sleep(blink_speed / 5)  # Off time    
-"""
-
+# Function error checking track playing
 def play_sound_effect(track_number):
     global current_track
     if track_number != current_track:
@@ -292,9 +274,6 @@ def draw_graph(oled, smoothed_value, history, window_size):
     
     oled.text('EMF: {:.2f}'.format(smoothed_value), 0, 0, 1)
     oled.text('Window: {:.2f}'.format(window_size), 0, 10, 1)
-#     description = classify_emf_reading(processed_value, baseline_emf_strength)
-#     oled.text('EMF: {:.2f}'.format(processed_value), 0, 0, 1)
-#     oled.text('SH: {:.2f}'.format(smoothed_history[-1] if smoothed_history else 0), 0, 10, 1)
 
     if history:
         # Assume SCALE_FACTOR is defined; adjust it based on your data's range
@@ -315,24 +294,23 @@ def draw_graph(oled, smoothed_value, history, window_size):
 
     oled.show()
 
-# Main loop
-# baseline = calibrate_sensor(sensor, oled, calibration_time=10)
-history = []
-increase_pressed = False
-decrease_pressed = False
+def main_loop():
+    global history  # If history is managed globally
+    while True:
+        check_and_handle_buttons()
+        emf_reading = sensor_manager.process_and_evaluate_emf_data()
 
-while True:
-    check_and_handle_buttons()
-    emf_reading = sensor_manager.process_sensor_data()
- 
-     if isinstance(processed_data, list) and len(processed_data) == 1:
-          emf_reading = processed_data[0]
-          
-    clamped_reading = max(EMF_MIN_ADC, min(EMF_MAX_ADC, emf_reading))
-         
-    led_controller.adjust_and_control_leds(clamped_reading)
-    servo_ramp.emf_to_servo_pos(clamped_reading)
-    play_track_based_on_emf(clamped_reading)
-    draw_graph(oled, clamped_reading, history, window_size)
+        led_controller.map_emf_to_speed(emf_reading)
+        servo_ramp.emf_to_servo_pos(emf_reading)
+        play_track_based_on_emf(emf_reading)
+       
+        if len(history) > window_size:
+            history.pop(0)
+            
+        draw_graph(oled, emf_reading, history, window_size)
+        time.sleep(0.2)
 
-    time.sleep(0.2)  # Adjust delay as needed
+if __name__ == "__main__":
+    history = []
+    sensor_manager.perform_calibration()
+    main_loop()
